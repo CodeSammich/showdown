f65602cc2ffafcf23607d417b9c75e71096f285f
michael <michaeljurado42@gmail.com>
2020-04-23 22:28:46 -0400

    simpleReward

--- showdown/battle_bots/nn_bot/DQNAgent.py
+++ showdown/battle_bots/nn_bot/DQNAgent.py
@@ -16,7 +16,7 @@ BUFFER_SIZE = int(1e4)  # replay buffer size
 BATCH_SIZE = 16  # minibatch size
 GAMMA = 0.90  # discount factor
 TAU = 1e-2  # for soft update of target parameters
-LR = 1e-4  # learning rate
+LR = 5e-4  # learning rate
 UPDATE_EVERY = 4  # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
