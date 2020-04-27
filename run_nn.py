640dcf2d69ab04ff8ec8a06126a705c6c9676506
Samuel Zhang <samuelzhang3@gmail.com>
2020-04-27 19:30:10 -0400

    increased episodes to 1000

--- run_nn.py
+++ run_nn.py
@@ -44,7 +44,7 @@ TRAIN = True
 """Training params"""
 num_games = 10
 TIMEOUT = 300 #seconds
-episodes = 500 
+episodes = 1000 
 merge_networks_time = 10000  # run this many times and then merge multiple agents TODO
 
 """Performance Params"""
